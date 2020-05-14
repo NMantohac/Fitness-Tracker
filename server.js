363e80afc6ef7d31c72ff20076d13e80dc17f630
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-13 17:55:41 -0700

    added process.env for port and mongoDB_URI

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const routes = require('./routes');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
 
-mongoose.connect('mongodb://localhost/redux_todo_db', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness_tracker_db', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.listen(PORT);
