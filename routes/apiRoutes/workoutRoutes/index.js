d23a1912e7b3ed32c63723e32fb075c19c1834ad
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-17 15:31:35 -0700

    completed initial workoutRoutes and htmlRoutes, and workout mongoose schema

--- routes/apiRoutes/workoutRoutes/index.js
+++ routes/apiRoutes/workoutRoutes/index.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
+const workoutController = require('../../../controllers/workoutController');
 
 // /api/workout prepended here
+
+// /api/workout
+router.route('/')
+  .get(workoutController.getLastWorkout)
+  .post(workoutController.createWorkout);
+
+// /api/workout/range
+router.route('/range')
+  .get(workoutController.getWorkoutsInRange);
+
+// /api/workout/:id
+router.route('/:id')
+  .put(workoutController.addExercise);
 
 module.exports = router;
